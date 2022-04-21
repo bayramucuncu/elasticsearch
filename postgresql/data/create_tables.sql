40adf65e73b77db2381bd2a4019ccea2f8401ceb
Bayram Üçüncü <ucuncubayram@gmail.com>
2022-04-21 12:10:48 +0300

    Postgresql data update.

--- postgresql/data/create_tables.sql
+++ postgresql/data/create_tables.sql
@@ -1,7 +1,12 @@
 -- Creation of product table
 CREATE TABLE IF NOT EXISTS products (
   product_id INT NOT NULL,
-  name varchar(250) NOT NULL,
+  product_name varchar(250) NOT NULL,
+  supplier_id INT NOT NULL,
+  supplier_name varchar(250) NOT NULL,
+  category_id INT NOT NULL,
+  category_name varchar(250) NOT NULL,
+  category_description varchar(250) NOT NULL,
   last_update_date timestamp NOT NULL DEFAULT NOW(),
   PRIMARY KEY (product_id)
 );
